d25f7857785ef74fd291352d4ecd108d3e98570f
unbeat01 <unbeatablesinha01@gmail.com>
2020-10-26 14:50:07 +0530

    Updates

---
+++ precache-manifest.f7666c15e84b559461e86c5ab15f4ff5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba4e32734a95c95d2fe8e7e86b2fe4ce",
+    "url": "/robosearch2/index.html"
+  },
+  {
+    "revision": "cdf1c032f568e8085126",
+    "url": "/robosearch2/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8e54c8dc382bac8aeb56",
+    "url": "/robosearch2/static/css/main.3b18601f.chunk.css"
+  },
+  {
+    "revision": "cdf1c032f568e8085126",
+    "url": "/robosearch2/static/js/2.710b911a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/robosearch2/static/js/2.710b911a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e54c8dc382bac8aeb56",
+    "url": "/robosearch2/static/js/main.ad87b922.chunk.js"
+  },
+  {
+    "revision": "bd0084b8e01d78371327",
+    "url": "/robosearch2/static/js/runtime-main.3b493bad.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robosearch2/static/media/SEGA.12a3db8b.woff"
+  }
+]);
